1ad463cd31f8feb96812523ebf8402ff375adba5
yasar budak <yasar.budak@hotmail.com>
2024-12-05 23:23:34 +0300

    Restructured project folder hierarchy and added orderService and ProductService

--- src/Presentation/ShopSphere.UserService.API/Services/TokenService.cs
+++ src/Presentation/ShopSphere.UserService.API/Services/TokenService.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 
 namespace ShopSphere.UserService.API.Services
 {
